3bb980dd8d3a80dbaee1be9cc9e83279f6d0212f
Sang Dang <sangdth@gmail.com>
2020-02-16 00:17:31 +0200

    UI looks ok .D

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,20 @@
 import React, {
-  useState,
-  useMemo,
+// useState,
+// useMemo,
 } from 'react';
 import c from 'classnames';
-import {
-  Col,
-  Row,
-  Select,
-} from 'antd';
 import Input from './components/Input';
 import './App.scss';
 
-const { Option } = Select;
-
-const App = () => {
-  const content = (
-    <div className="menu">
-      <Row type="flex" align="middle" gutter={[10, 10]}>
-        <Col span={8} className="label-col">Key:</Col>
-        <Col span={16}>
-          <Input />
-        </Col>
-      </Row>
-
-      <Row type="flex" align="middle" gutter={[10, 10]}>
-        <Col span={8} className="label-col">Type:</Col>
-        <Col span={16}>
-          <Select
-            // defaultValue={value.type}
-            style={{ width: 120 }}
-            // onChange={(t: string) => setValue({ ...value, type: t })}
-          >
-            <Option value="none"> – </Option>
-            <Option value="select">select</Option>
-            <Option value="plural">plural</Option>
-          </Select>
-        </Col>
-      </Row>
-
-      <Row type="flex" align="middle" gutter={[10, 10]}>
-        <Col span={8} className="label-col">
-          Match / Output
-        </Col>
-        <Col span={16}>
-          <Row type="flex" align="middle" gutter={[5, 5]}>
-            <Col span={12}>
-              <Input />
-            </Col>
-            <Col span={12}>
-              <Input />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </div>
-  );
-
-  return (
-    <div className="App">
-      <Input onClick={(o: any) => console.log({ o })} />
-      <div className={c('select-area', 'block')} />
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <h2>Message Syntax Generator</h2>
+    <Input
+      root
+      onChange={(o: any) => console.log({ o })}
+    />
+    <div className={c('select-area', 'block')} />
+  </div>
+);
 
 export default App;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,76 +1,228 @@
 import React, {
+  createRef,
   useCallback,
+  useEffect,
   useMemo,
   useState,
 } from 'react';
 import {
-  // Col,
+  Button,
+  Checkbox,
+  Col,
   Input as AntInput,
   // Popover,
-  // Row,
-  // Select,
+  Row,
+  Select,
 } from 'antd';
+import { usePrevious } from 'react-use';
+import { isEqual } from 'lodash';
 import PropTypes from 'prop-types';
 import c from 'classnames';
 import './styles.scss';
 
+const { Option } = Select;
+
+interface Word {
+  key: string
+  value: string
+  index: number
+  sub: Word | undefined
+}
+
 const Input = (props: any) => {
-  const { onClick } = props;
+  const { onChange, onClick, root, value } = props;
 
   const [text, setText] = useState('');
-  const [selected, setSelected] = useState({
-    key: '',
-    value: '',
-    index: 0,
-  });
+  const [selected, setSelected] = useState(value);
 
-  const words = useMemo(() => text.split(' ')
-    .filter((s) => s)
-    .map((w, i) => ({
-      index: i,
-      key: `${i}-${Math.random().toString(36).slice(-5)}`,
-      value: w,
-    })), [text]);
+  const initialNested = useMemo(
+    () => {
+      if (selected) {
+        return !!selected.sub;
+      }
+      return false;
+    },
+    [selected],
+  );
 
-  const wordClick = useCallback((e: any, s: any) => {
-    setSelected(s);
-    if (onClick) {
-      onClick(e);
-    }
-  }, [onClick]);
+  const [nested, setNested] = useState(initialNested);
+
+  const words = useMemo(
+    () => text.split(' ')
+      .filter((s: string) => s)
+      .map((s: string, i: number) => ({
+        index: i,
+        key: `${i}-${Math.random().toString(36).slice(-5)}`,
+        value: s,
+        sub: undefined,
+      })),
+    [text],
+  );
+
+  const refs = useMemo(
+    () => words.map(() => createRef<HTMLSpanElement>()),
+    [words],
+  );
+
+  const arrowPosition = useMemo(
+    () => {
+      if (selected) {
+        const { index } = selected;
+        if (refs && refs[index]) {
+          const { current } = refs[index];
+          if (current) {
+            const { x, left, width } = current.getBoundingClientRect();
+            console.log({ x, left });
+            return left + width / 2 - 20;
+          }
+        }
+      }
+      return 0;
+    },
+    [refs, selected],
+  );
+
+  const wordClick = useCallback(
+    (event: any, word: Word) => {
+      if (typeof onClick === 'function') {
+        onClick(event);
+      }
+      if (!selected || selected.key !== word.key) {
+        setSelected(word);
+      } else {
+        setSelected(undefined);
+      }
+    },
+    [onClick, selected],
+  );
+
+  const prevSelected = usePrevious(selected);
+  useEffect(
+    () => {
+      if (prevSelected && selected && onChange) {
+        if (!isEqual(selected, prevSelected)) {
+          onChange(selected, words);
+        }
+      }
+    },
+    [onChange, prevSelected, selected, words],
+  );
 
   return (
-    <div>
-      <AntInput
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+    <>
+      <Row type="flex" align="middle" gutter={[10, 10]}>
+        {!root && (
+          <>
+            <Col span={2} offset={1} className="label-col">
+              Match:
+            </Col>
+            <Col span={4}>
+              <AntInput />
+            </Col>
+            <Col span={2} offset={10}>
+              <Checkbox
+                checked={nested}
+                className="checkbox"
+                onChange={(e: any) => setNested(e.target.checked)}
+              >
+                Nested
+              </Checkbox>
+            </Col>
+            <Col span={3} style={{ textAlign: 'right' }}>
+              <Button type="primary" icon="plus">
+                Add match
+              </Button>
+            </Col>
+          </>
+        )}
+      </Row>
 
-      <div className="text">
-        {words.map(({ index, key, value }) => (
-          <span
-            role="presentation"
-            className={c('word', value, {
-              selected: selected && key === selected.key,
-            })}
-            onClick={(e) => wordClick(e, { index, key, value })}
-          >
-            {value}
-          </span>
-        ))}
+      <Row type="flex" align="middle" gutter={[10, 10]}>
+        {!root && (
+          <Col span={2} offset={1} className="label-col">
+            Output:
+          </Col>
+        )}
+        <Col span={root ? 24 : 19}>
+          <AntInput
+            value={text}
+            size={root ? 'large' : undefined}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </Col>
+      </Row>
+
+      {(root || nested) && text && (
+      <Row className="text" type="flex" align="middle" gutter={[10, 10]}>
+        <Col span={24} offset={root ? 0 : 3}>
+          {words.map((word: Word) => (
+            <span
+              className={c('word', word.key, {
+                selected: selected && word.key === selected.key,
+              })}
+              key={word.key}
+              onClick={(event) => wordClick(event, word)}
+              ref={refs[word.index]}
+              role="presentation"
+            >
+              {word.value}
+            </span>
+          ))}
+        </Col>
+      </Row>
+      )}
+
+      {(root || nested) && selected && (
+      <div className={c('actions', { root })}>
+        {selected && (
+          <div className="my-arrow" style={{ left: arrowPosition }} />
+        )}
+
+        <Row type="flex" align="middle" gutter={[10, 10]}>
+          <Col span={2} offset={1} className="label-col">
+            Key:
+          </Col>
+          <Col span={4}>
+            <AntInput />
+          </Col>
+
+          <Col span={2} offset={1} className="label-col">
+            Type:
+          </Col>
+          <Col span={4}>
+            <Select
+              className="select-options"
+              defaultValue="none"
+            >
+              <Option value="none"> – </Option>
+              <Option value="select">select</Option>
+              <Option value="plural">plural</Option>
+            </Select>
+          </Col>
+        </Row>
+
+        <Input
+          value={selected.sub}
+          onChange={(o: any) => setSelected({ ...selected, sub: o })}
+        />
       </div>
-    </div>
+      )}
+    </>
   );
 };
 
 Input.PropsTypes = {
   onChange: PropTypes.func,
   onClick: PropTypes.func,
+  root: PropTypes.bool,
+  value: PropTypes.object,
 };
 
 Input.defaultProps = {
   onChange: null,
   onClick: null,
+  root: false,
+  value: undefined,
 };
 
 export default Input;
