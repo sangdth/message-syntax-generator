9d808d92bda00ce134e08bfa9cbcc5337a850bec
Sang Dang <sangdth@gmail.com>
2020-03-06 20:58:23 +0200

    wow not crash

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -56,7 +56,14 @@ const Input = (props: any) => {
         };
       }
     }
-    return { key: '', type: '', matches: [{ match: '' }] };
+    return {
+      key: '',
+      type: '',
+      matches: [{
+        match: '',
+        output: { value: '', syntaxes: [] },
+      }],
+    };
   }, [selected, syntaxes]);
 
   const [key, setKey] = useState(initial.key);
@@ -93,6 +100,12 @@ const Input = (props: any) => {
       setLock(false);
     }
   }, [selected]);
+
+  const setOutput = useCallback((out: Output, index: number) => {
+    const tmpMatches = [...matches];
+    tmpMatches.splice(index, 1, { ...matches[index], output: out });
+    setMatches(tmpMatches);
+  }, [matches]);
 
   /* -------------------------------------------------------------------
    * Handling side effects
@@ -140,7 +153,10 @@ const Input = (props: any) => {
       } else {
         setKey('');
         setType('');
-        setMatches([{ match: '' }]);
+        setMatches([{
+          match: '',
+          output: { value: '', syntaxes: [] },
+        }]);
       }
     }
   }, [selected, syntaxes]);
@@ -156,130 +172,132 @@ const Input = (props: any) => {
 
   /* -------------------------------------------------------------------
    * Rendering
-  return matches.map((match: Match, index: number) => (
-    <Fragment key={match.match}>
-      <Row type="flex" align="middle" gutter={[10, 10]}>
-        {!root && (
-          <>
-            <Col span={2} offset={1} className="label-col">
-              Match:
-            </Col>
-            <Col span={4}>
-              <AntInput
-                value={match.match}
-                onChange={(e: any) => handleMatches(m.id, 'match', e.target.value)}
-              />
-            </Col>
-            <Col span={2} offset={10}>
-              <Checkbox
-                checked={!!m.subData}
-                className="checkbox"
-                onChange={(e: any) => handleNestedMatch(m, e.target.checked)}
-              >
-                Nested
-              </Checkbox>
-            </Col>
-            <Col span={3} style={{ textAlign: 'right' }}>
-              {matches.length - 1 === i ? (
-                <Button
-                  disabled={!m.match}
-                  type="primary"
-                  icon="plus"
-                  onClick={addBlankMatch}
-                />
-              ) : (
-                <Button
-                  icon="minus"
-                  onClick={() => {}}
-                />
-              )}
-            </Col>
-          </>
-        )}
-      </Row>
-          <Input
-            root={false}
-            output={format.output}
-            onChange={() => {}}
-          />
-
    */
   return (
     <>
-      <Row type="flex" align="middle" gutter={[10, 10]}>
-        {!root && (
-          <Col span={2} offset={1} className="label-col">
-            Output:
-          </Col>
-        )}
-        <Col span={root ? 24 : 19}>
-          <AntInput
-            disabled={lock}
-            value={text}
-            size={root ? 'large' : undefined}
-            onChange={(e: any) => setText(e.target.value)}
-          />
-        </Col>
-      </Row>
+      {matches.map((match: Match, index: number) => (
+        <Fragment key={match.match}>
+          <Row type="flex" align="middle" gutter={[10, 10]}>
+            {!root && (
+              <>
+                <Col span={2} offset={1} className="label-col">
+                  Match:
+                </Col>
+                <Col span={4}>
+                  <AntInput
+                    value={match.match}
+                  />
+                </Col>
+                <Col span={2} offset={10}>
+                  <Checkbox
+                    checked={!!match.output}
+                    className="checkbox"
+                  >
+                    Nested
+                  </Checkbox>
+                </Col>
+                <Col span={3} style={{ textAlign: 'right' }}>
+                  {matches.length - 1 === index ? (
+                    <Button
+                      disabled={!match.match}
+                      type="primary"
+                      icon="plus"
+                    />
+                  ) : (
+                    <Button
+                      icon="minus"
+                      onClick={() => {}}
+                    />
+                  )}
+                </Col>
+              </>
+            )}
+          </Row>
 
-      {text && (
-      <Row className="text" type="flex" align="middle" gutter={[10, 10]}>
-        <Col span={24} offset={root ? 0 : 3}>
-          {words.map((word: Word) => (
-            <span
-              className={c('word', word.value, {
-                selected: selected && word.index === selected.index,
-              })}
-              key={word.index}
-              onClick={() => wordClick(word)}
-              ref={refs[word.index]}
-              role="presentation"
-            >
-              {word.value}
-            </span>
-          ))}
-        </Col>
-      </Row>
-      )}
+          <Row type="flex" align="middle" gutter={[10, 10]}>
+            {!root && (
+            <Col span={2} offset={1} className="label-col">
+              Output:
+            </Col>
+            )}
+            <Col span={root ? 24 : 19}>
+              <AntInput
+                disabled={lock}
+                value={text}
+                size={root ? 'large' : undefined}
+                onChange={(e: any) => setText(e.target.value)}
+              />
+            </Col>
+          </Row>
 
-      {selected && (
-      <div className={c('actions', { root })}>
-        {selected && (
-          <div className="my-arrow" style={{ left: arrowPosition }} />
-        )}
-
-        <Row type="flex" align="middle" gutter={[10, 10]}>
-          <Col span={2} offset={1} className="label-col">
-            Key:
-          </Col>
-          <Col span={4}>
-            <AntInput
-              value={key}
-              onChange={(event: any) => setKey(event.target.value)}
-            />
-          </Col>
-
-          <Col span={2} offset={1} className="label-col">
-            Type:
-          </Col>
-          <Col span={4}>
-            <Select
-              className="select-options"
-              defaultValue=""
-              value={type}
-              onChange={(syntaxType: string) => setType(syntaxType)}
-            >
-              <Option value=""> – </Option>
-              {syntaxTypes.map((syntaxType: string) => (
-                <Option key={syntaxType} value={syntaxType}>
-                  {syntaxType}
-                </Option>
+          {text && (
+          <Row className="text" type="flex" align="middle" gutter={[10, 10]}>
+            <Col span={24} offset={root ? 0 : 3}>
+              {words.map((word: Word) => (
+                <span
+                  className={c('word', word.value, {
+                    selected: selected && word.index === selected.index,
+                  })}
+                  key={word.index}
+                  onClick={() => wordClick(word)}
+                  ref={refs[word.index]}
+                  role="presentation"
+                >
+                  {word.value}
+                </span>
               ))}
-            </Select>
-          </Col>
-        </Row>
-      </div>
-      )}
+            </Col>
+          </Row>
+          )}
+
+          {(root || !!match.output) && selected && (
+          <div className={c('actions', { root })}>
+            {selected && (
+            <div className="my-arrow" style={{ left: arrowPosition }} />
+            )}
+
+            <Row type="flex" align="middle" gutter={[10, 10]}>
+              <Col span={2} offset={1} className="label-col">
+                Key:
+              </Col>
+              <Col span={4}>
+                <AntInput
+                  value={key}
+                  onChange={(event: any) => setKey(event.target.value)}
+                />
+              </Col>
+
+              <Col span={2} offset={1} className="label-col">
+                Type:
+              </Col>
+              <Col span={4}>
+                <Select
+                  className="select-options"
+                  defaultValue=""
+                  value={type}
+                  onChange={(syntaxType: string) => setType(syntaxType)}
+                >
+                  <Option value=""> – </Option>
+                  {syntaxTypes.map((syntaxType: string) => (
+                    <Option key={syntaxType} value={syntaxType}>
+                      {syntaxType}
+                    </Option>
+                  ))}
+                </Select>
+              </Col>
+            </Row>
+
+            {match.output && (
+              <Input
+                root={false}
+                output={match.output}
+                onChange={(out: Output) => setOutput(out, index)}
+              />
+            )}
+          </div>
+          )}
+        </Fragment>
+      ))}
     </>
   );
 };
